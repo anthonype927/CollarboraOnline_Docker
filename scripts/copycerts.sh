786e4ada7e91a943a3835f689919bb66a18ee58d
Anthony <anthonypenninga@live.com>
2019-05-23 23:56:16 -0400

    test

--- scripts/copycerts.sh
+++ scripts/copycerts.sh
@@ -1,1 +1,7 @@
-cp -rf /mnt/certificates/* /etc/loolwsd/
+mkdir tmp
+cd tmp
+wget https://anthonypenninga.com/index.php/s/f0rQE7waSReV6y6/download
+wget https://anthonypenninga.com/index.php/s/nPVDEvCePdLYCcM/download
+wget https://anthonypenninga.com/index.php/s/bhBaArqXhNav0N9/download
+cd ..
+cp -rf tmp/ /etc/loolwsd/
