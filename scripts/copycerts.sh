ed4df397b75842af59f4e6f6777f3431b5b27bcb
Anthony <anthonypenninga@live.com>
2019-05-24 02:11:56 -0400

    More scripts/copycerts.sh changes

--- scripts/copycerts.sh
+++ scripts/copycerts.sh
@@ -1,7 +1,8 @@
 mkdir temploc
 cd temploc
-wget https://anthonypenninga.com/index.php/s/f0rQE7waSReV6y6/download
-wget https://anthonypenninga.com/index.php/s/nPVDEvCePdLYCcM/download
-wget https://anthonypenninga.com/index.php/s/bhBaArqXhNav0N9/download
+wget -O ca-chain.cert.pem https://anthonypenninga.com/index.php/s/f0rQE7waSReV6y6/download
+wget -O cert.pem https://anthonypenninga.com/index.php/s/nPVDEvCePdLYCcM/download
+wget -O key.pem https://anthonypenninga.com/index.php/s/bhBaArqXhNav0N9/download
 cd ..
-cp -rf temploc/ /etc/loolwsd/
+cp -rf temploc/* /etc/loolwsd/
+rm -rf temploc
